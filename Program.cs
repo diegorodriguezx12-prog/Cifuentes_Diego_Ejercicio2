a0ccc1b3e90b2d0de89b35302f733adbfb9dc9be
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-02-22 13:34:03 -0600

    Proyecto creado

--- Program.cs
+++ Program.cs
@@ -1,1 +1,4 @@
-﻿
+﻿Console.WriteLine("=== Control de Acceso a Laboratorio Universitario ===");
+Console.WriteLine("Seleccione su Rol:");
+Console.WriteLine("Estudiante(1) -- Docente(2) -- Técnico IT(3) -- Visitante(4)");
+Console.Write("Opción: ");
